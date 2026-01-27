46b10ec6a706ffd94eac7f9ef9e3cd056f86565b
harshchhabraa <harshchhabra0512@gmail.com>
2026-01-28 01:23:11 +0530

    added categories component

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import { ProductType } from "@/types"
+
+const ProductCard = ({product} : {product:ProductType}) => {
+    return(
+        <div className="">ProductCard</div>
+    )
+}
+export default ProductCard
